75d33ab66d1d8a4644375863861c36ff9a7acaac
alokkumar0987 <alok33778@gmail.com>
2025-08-17 00:13:31 +0530

    hi

---
+++ practices_small_project/pro_00.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+if not GROQ_API_KEY:
+    raise ValueError("GROQ_API_KEY is not set in the environment variables. Please set it in your .env file.")
+
+from langchain_groq import ChatGroq
+
+llm = ChatGroq(
+    model="deepseek-r1-distill-llama-70b",
+    temperature=0,
+    max_tokens=None,
+    reasoning_format="parsed",
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
